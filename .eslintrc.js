e7f307bc7bf070e8b385e15401308decfdbfb974
Sharukh-maker <mohammad.sharukh@aurora.sbs.nhs.uk>
2023-10-03 00:54:04 +0530

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
-require('@rushstack/eslint-config/patch/modern-module-resolution');
+// require('@rushstack/eslint-config/patch/modern-module-resolution');
+// module.exports = {
+//   extends: ['@microsoft/eslint-config-spfx/lib/profiles/default'],
+//   parserOptions: { tsconfigRootDir: __dirname },
+//   "ecmaVersion":6
+// };
 module.exports = {
-  extends: ['@microsoft/eslint-config-spfx/lib/profiles/default'],
-  parserOptions: { tsconfigRootDir: __dirname },
-  "ecmaVersion":6
+  // ...
+  "parserOptions": {
+    "ecmaVersion": 2021, // Specify the ECMAScript version here
+    // You can also include other parser options here if needed
+  },
+  // ...
 };
